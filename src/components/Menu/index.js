fba70bbfd76dc0a2174898aa580ec670e0a04039
Matheus Lima <matheus_sousa@protonmail.com>
2020-07-28 21:43:42 -0300

    mudando height do logo

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
 
 function Menu() {
     return (
         <nav>
-            <img src={Logo}/>
+            <img class="Logo" src={Logo} alt="AluraFlix logo"/>
         </nav>
     );
 }
